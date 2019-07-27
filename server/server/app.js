528eb1e86292b884dad0fcc4fcb7cab0608a7d0f
endorphin82 <endorphin82@gmail.com>
2019-07-27 19:32:12 +0300

    07-add_mongoose_update_models

--- server/server/app.js
+++ server/server/app.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("../schema/schema");
+const mongoose = require("mongoose");
+
 
 const app = express();
 const PORT = 3005;
+
+mongoose.connect("mongodb://air:air123@ds343217.mlab.com:43217/graphql-tutorial",
+  // { useMongoClient: true } // old
+  { useNewUrlParser: true }, // new mongoose v5+
+);
+
 
 app.use("/graphql", graphqlHTTP({
   schema,
   graphiql: true,
 }));
+
+const dbConnection = mongoose.connection;
+dbConnection.on("error", err => {
+  console.log(`Connection error: ${err}`);
+});
+dbConnection.once("open", () => {
+  console.log("Connected to DB");
+});
 
 app.listen(PORT, err => {
   err ? console.log(err) : console.log(`Server started on PORT ${PORT}`);
